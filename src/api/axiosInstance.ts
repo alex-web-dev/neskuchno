6698a39c99bb01faf2ad99d15e6b7e78e1f1579a
alex-web-dev <alex.web-dev@mail.ru>
2025-03-03 18:57:28 +0300

    feat: update api url

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://api.nesqu123.site/api/',
+  baseURL: 'https://api.nesqu123.site/api/',
   headers: {
     'Content-Type': 'application/json',
   },
